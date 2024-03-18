9a52a13345be0bbb92f79f664ceb17ee11157fb2
IZ <Ilyazotovspb@gmail.com>
2024-03-18 18:24:02 +0300

    todo

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+
+const ToDoList = () => {
+  return 
+}
+
+const AppHeader = () => {
+  return
+}
+
+const SearchPanel = () => {
+  return
+}
+
+const el = <h1>Hello</h1>;
+console.log(el);
+
+const root = createRoot(document.querySelector('section'));
+root.render(el);
